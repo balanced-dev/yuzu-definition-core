5ac25a9361e976fa593186c704429e818f7822e6
Phil Whittaker <phil@hifi.agency>
2019-04-25 11:32:30 +0100

    build data only method added

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var findTemplateAndValidateFromJson = require('./modules/findTemplateAndValidateFromJson');
 var findTemplateAndValidateFromLayout = require('./modules/findTemplateAndValidateFromLayout');
 var registerHandlebarsPartials = require('./modules/registerHandlebarsPartials');
+var buildData = require('./modules/buildData');
 var getExternals = require('./modules/getExternals');
 var gulpSassJson = require('./modules/hifi-gulp-sass-json');
 
 module.exports.findTemplateAndValidateFromJson = findTemplateAndValidateFromJson;
 module.exports.findTemplateAndValidateFromLayout = findTemplateAndValidateFromLayout;
 module.exports.registerHandlebarsPartials = registerHandlebarsPartials;
+module.exports.buildData = buildData;
 module.exports.getExternals = getExternals;
 module.exports.gulpSassJson = gulpSassJson;
