295b207dd5a49819c782680305c966ec6f87977f
Phil Whittaker <phil@hifi.agency>
2019-07-26 15:50:14 +0100

    fix block hihlighting so it doesn'l randomly add double quotes

--- modules/services/blockHighlightService.js
+++ modules/services/blockHighlightService.js
@@ -2,7 +2,7 @@ const property = 'yuzu-path';
 
 var addYuzuMarker = function(template) {
 
-	return template.replace(/<\w*\s/, '$&{{#if '+ property +'}}data-yuzu="{{'+ property +'}}{{/if}}" ');
+	return template.replace(/<\w*\s/, '$&{{#if '+ property +'}}data-yuzu="{{'+ property +'}}"{{/if}} ');
 }
 
 module.exports.property = property;
